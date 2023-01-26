3c9c7362f35059f58a17b9b654bbe640277595ec
socrampontes <114445301+socrampontes@users.noreply.github.com>
2023-01-26 20:11:58 -0300

    template-componentes

---
+++ template/src/componentes/footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import {Copy} from "./copywrite";
+import {Redes} from "./redes-sociais";
+
+ export function footer () {
+  return (
+    <div>
+     <footer>
+        <Copy></Copy>
+        <Redes></Redes>
+
+     </footer>
+    </div>
+  );
+}
